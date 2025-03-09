32ffc2c03e95546c57ed0e17db8e3a21d15ebc79
sabister <blackmagejr1@gmail.com>
2025-03-09 13:32:34 -0400

    first commit

---
+++ lab1.py
@@ -0,0 +1,15 @@
+##Sample coding questions 01 week 01
+## Sabannah De-Gale
+array = {1,4,7,9}
+
+a=1
+b=2
+c=3
+d=4
+
+e = (a - ((b ** c) // d)) + (a % c)
+
+temperature=  32.6
+print("The temperature today is: "+ temperature +" degrees Celsius")
+userAge = input("What is your age? ")
+print( "Now showing the shop items filtered by age:"+ userAge )
