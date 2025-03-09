9ab46c52004a4706cc7e2b08a74edb07f8a7a519
sabister <blackmagejr1@gmail.com>
2025-03-09 17:13:29 -0400

    Lab 5 complete

---
+++ functions_lab05.py
@@ -0,0 +1,89 @@
+# Import the random library to use for the dice later
+import random
+
+# Will the line below print when you import function.py into main.py?
+# print("Inside function.py")
+
+# Hero's Attack Function
+def hero_attacks(combat_strength, m_health_points):
+    ascii_image = """
+                                @@   @@ 
+                                @    @  
+                                @   @   
+               @@@@@@          @@  @    
+            @@       @@        @ @@     
+           @%         @     @@@ @       
+            @        @@     @@@@@     
+               @@@@@        @@       
+               @    @@@@                
+          @@@ @@                        
+       @@     @                         
+   @@*       @                          
+   @        @@                          
+           @@                                                    
+         @   @@@@@@@                    
+        @            @                  
+      @              @                  
+
+  """
+    print(ascii_image)
+    print("    |    Player's weapon (" + str(combat_strength) + ") ---> Monster (" + str(m_health_points) + ")")
+    if combat_strength >= m_health_points:
+        # Player was strong enough to kill monster in one blow
+        m_health_points = 0
+        print("    |    You have killed the monster")
+    else:
+        # Player only damaged the monster
+        m_health_points -= combat_strength
+
+        print("    |    You have reduced the monster's health to: " + str(m_health_points))
+    return m_health_points
+
+
+# Monster's Attack Function
+def monster_attacks(m_combat_strength, health_points):
+    ascii_image2 = """                                                                 
+           @@@@ @                           
+      (     @*&@  ,                         
+    @               %                       
+     &#(@(@%@@@@@*   /                      
+      @@@@@.                                
+               @       /                    
+                %         @                 
+            ,(@(*/           %              
+               @ (  .@#                 @   
+                          @           .@@. @
+                   @         ,              
+                      @       @ .@          
+                             @              
+                          *(*  *      
+             """
+    print(ascii_image2)
+    print("    |    Monster's Claw (" + str(m_combat_strength) + ") ---> Player (" + str(health_points) + ")")
+    if m_combat_strength >= health_points:
+        # Monster was strong enough to kill player in one blow
+        health_points = 0
+        print("    |    Player is dead")
+    else:
+        # Monster only damaged the player
+        health_points -= m_combat_strength
+        print("    |    The monster has reduced Player's health to: " + str(health_points))
+    return health_points
+
+def collect_loot(belt, loot_options):
+    print("Collecting Loot...")
+    belt.extend(loot_options)
+    return [belt, loot_options]
+
+def use_loot(belt, health_points):
+    print("Using Loot...")
+    if "health_potion" in belt:
+        health_points += 10
+        belt.remove("health_potion")
+    return [belt, health_points]
+
+def inception_dream(crazy_level=0):
+    choice = input("Do you want to go deeper into the dream? (yes/no): ")
+    if choice.lower() == "yes":
+        return 1 + inception_dream(crazy_level + 1)
+    return 2
