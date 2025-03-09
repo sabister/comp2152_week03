21df5a1318c8272cbe510464b30fdc9a8592c403
sabister <blackmagejr1@gmail.com>
2025-03-09 14:00:27 -0400

    updated

--- lab02.py
+++ lab02.py
@@ -1,4 +1,3 @@
-# Import the random library to use for the dice later
 import random
 
 # Define Variables
@@ -6,75 +5,34 @@ numLives = 10           # number of player's lives remaining
 mNumLives = 12          # number of monster's lives remaining
 
 diceOptions = [1, 2, 3, 4, 5, 6]
-combatStrength = int(input("Enter your combat Strength: "))
-mCombatStrength = int(input("Enter the monster's combat Strength: "))
+weapons = ["Fist", "Knife", "Club", "Gun", "Bomb", "Nuclear bomb"]
 
-input("Roll the dice for your health points (Press enter)")
-healthPoints = random.choice(diceOptions)
-print("You rolled " + str(healthPoints) + " health points")
-
-input("Roll the dice for the monster's health points (Press enter)")
-mHealthPoints = random.choice(diceOptions)
-print("You rolled " + str(mHealthPoints) + " health points for the monster")
-
-input("Roll the dice to see if you find a healing potion (Press enter)")
-healingPotion = random.choice([0, 1])
-print("Have you found a healing potion?: " + str(bool(healingPotion)))
-
-input("Analyze the roll (Press enter)")
-# Equality operators
-print("--- You are matched in strength: " + str(combatStrength == mCombatStrength))
-
-# Relational operators
-print("--- You have a strong player: " + str((combatStrength + healthPoints) >= 15))
-
-# and keyword
-print("--- Remember to take a healing potion!: " + str(healingPotion == 1 and healthPoints <= 6))
-
-# not keyword
-print("--- Phew, you have a healing potion: " + str(
-    not (                               # monster will NOT kill hero in one blow
-        healthPoints < mCombatStrength  # monster will kill hero in one blow
-    )
-    and
-    healingPotion == 1                  # hero has a healing potion
-))
-
-# or keyword
-print("--- Things are getting dangerous: " + str(healingPotion == 0 or healthPoints == 1))
-
-# in keyword
-print("--- Is it possible to roll 0 in the dice?: " + str(0 in diceOptions))
-
-# --- Expanded if statement
-if healthPoints >= 5:
-    print("--- Your health is ok")
-elif healingPotion == 1:
-    healingPotion = 0
-    healthPoints = 6
-    print("--- Using your healing potion... Your Health Points is now full at " + str(healthPoints))
+# Get hero's combat strength
+combatStrength = input("Enter your combat Strength (1-6): ")
+if combatStrength.isdigit():
+    combatStrength = int(combatStrength)
+    if combatStrength < 1 or combatStrength > 6:
+        print("Invalid input! Combat strength must be between 1 and 6.")
+        exit()
 else:
-    print("--- Your health is low at " + str(healthPoints) + " and you have no healing potions available!")
+    print("Invalid input! Please enter a number between 1 and 6.")
+    exit()
 
+# Roll the dice for weapon selection
+input("Roll the dice for your weapon (Press enter)")
+weaponRoll = random.choice(diceOptions) - 1  # Adjusting index to 0-based
+heroWeapon = weapons[weaponRoll]
+combatStrength += weaponRoll + 1  # Adding weapon strength to combat strength
 
-# --- Nested if statement
-print("You meet the monster. FIGHT!!")
-input("You strike first (Press enter)")
+print(f"You rolled {weaponRoll + 1} and got {heroWeapon}")
 
-print("Your sword (" + str(combatStrength) + ") ---> Monster (" + str(mHealthPoints) + ")")
-if combatStrength >= mHealthPoints:
-    mHealthPoints = 0
-    print("You've killed the monster")
+# Evaluate weapon strength
+if weaponRoll <= 1:
+    print("You rolled a weak weapon, friend.")
+elif weaponRoll <= 3:
+    print("Your weapon is meh.")
 else:
-    mHealthPoints -= combatStrength
+    print("Nice weapon, friend!")
 
-    print("You've reduced the monster's health to: " + str(mHealthPoints))
-
-    print("The monster strikes!!!")
-    print("Monster's Claw (" + str(mCombatStrength) + ") ---> You (" + str(healthPoints) + ")")
-    if mCombatStrength >= healthPoints:
-        healthPoints = 0
-        print("You're dead")
-    else:
-        healthPoints -= mCombatStrength
-        print("The monster has reduced your health to: " + str(healthPoints))
+if heroWeapon != "Fist":
+    print("Thank goodness you didn't roll the Fist...")
